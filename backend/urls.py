34f981d45fc001a8dd0d8efcecfbd5fad751b361
aabdurakhmanov <aabdurakhmanov98@gmail.com>
2023-10-20 18:47:17 +0500

    add drf api-auth

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', views.TaskList.as_view(), name='task-list'),
     path('<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
 ]
+
