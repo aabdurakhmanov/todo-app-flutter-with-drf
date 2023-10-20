c63aa8b68e390639cc7a352f329f9e6f1e8d2671
aabdurakhmanov <aabdurakhmanov98@gmail.com>
2023-10-20 23:19:22 +0500

    add drf api-auth

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend.urls')),
+    path('v1/', include('backend.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
