7816a32f70ca8a9a3f10cc29e4b3e5093963eb4f
aabdurakhmanov <aabdurakhmanov98@gmail.com>
2023-10-22 22:36:51 +0500

    add drf api-auth

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', views.TaskList.as_view(), name='task-list'),
-    path('<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
+    path('todo/', views.TaskList.as_view(), name='task-list'),
+    path('todo/<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
 ]
 
