7631ab86ea1a920b642603384cc433c6aa3e2faf
aabdurakhmanov <aabdurakhmanov98@gmail.com>
2023-10-23 18:43:00 +0500

    front end qoshildi

---
+++ todo_frontend/lib/screens/add_page.dart
@@ -0,0 +1,95 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Todo'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 100,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: submitData,
+            child: Text('Submit'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "status": false,
+    };
+
+    final url = 'http://127.0.0.1:8000/v1/todo';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      showSuccessMessage('Creation Success');
+    } else {
+      showErrorMessage('Creation Failed');
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
---
+++ todo_frontend/lib/screens/todo_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quotes/screens/add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAddPage,
+        label: const Text('Add Todo'),
+      ),
+    );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+}
